522266e70730138272e50e2d921d9bf0eefeb5bb
Yonathan <yonabaho@gmail.com>
2026-08-27 15:49:07 +0700

    Create item form page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './commons/Layout'
-import ItemPage from './pages/item/ItemPage'
-import ItemDetailPage from './pages/item/ItemDetailPage'
 import ItemEditPage from './pages/item/ItemEditPage'
+import ItemFormPage from './pages/item/ItemFormPage'
+import ItemPage from './pages/item/ItemPage'
 import UserPage from './pages/UserPage'
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/item" element={<ItemPage />} />
-          <Route path="/item/:id" element={<ItemDetailPage />} />
-          <Route path="/item/:id/form" element={<ItemEditPage />} />
+          <Route path="/item/new" element={<ItemFormPage />} />
+          <Route path="/item/:id/edit" element={<ItemEditPage />} />
           <Route path="/user" element={<UserPage />} />
           <Route path="*" element={<Navigate to="/item" replace />} />
         </Route>
---
+++ src/pages/item/ItemFormPage.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { Box, Button, TextField } from '@mui/material'
+import SaveIcon from '@mui/icons-material/Save'
+import { API_ITEM } from '../../constants/Url'
+import Title from '../../commons/Title'
+
+function ItemFormPage() {
+  const [item, setItem] = useState({ name: '' })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItem({ ...item, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async () => {
+    try {
+      await fetch(API_ITEM, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: item.name }),
+      })
+    } catch (error) {
+      console.error('Failed to save item:', error)
+    }
+  }
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Title titleText="Item Form" />
+      <Box sx={{ p: 3 }}>
+        <TextField
+          fullWidth
+          label="Name"
+          name="name"
+          value={item.name}
+          onChange={handleChange}
+          sx={{ mb: 2 }}
+        />
+        <Button
+          variant="contained"
+          startIcon={<SaveIcon />}
+          onClick={handleSubmit}
+          sx={{ bgcolor: 'green', '&:hover': { bgcolor: 'darkgreen' } }}
+        >
+          Save
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default ItemFormPage
